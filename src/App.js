19b80016e0c2f37bace52e91dc24adb6571149c7
Harshda Mhase <harshadamhase12@gmail.com>
2023-11-05 07:28:29 -0500

    wether app completed

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className="App">
-     <h1 className="text-center mt-2">🌦️ Weather App 🌦️</h1>
+     <h1 className="text-center">🌦️ Weather App 🌦️</h1>
 
      <input
           type="text"
@@ -45,23 +45,24 @@ function App() {
             setCity(e.target.value);
           }}
         />
-       <h2 className="mt-3">{weatherData.name}</h2>
+       <h1 className="mt-3">{weatherData.name}</h1>
         <h2> Description : {weatherDescription}</h2>
-        {/* <h1 className="font">⛈</h1> */}
         
         <div className="d-flex justify-content-evenly mt-5">
 
-          <h3 className="temperature-card shadow">
+          <h3 className="text">
             Temp =
-            {`${(weatherData?.main?.temp - 273).toFixed(2)}°C`} 
+             {`${(weatherData?.main?.temp - 273).toFixed(2)}°C`} 
           </h3>
 
-          <h3 className="temperature-card shadow ">
-             Visibility = {`${weatherData?.visibility}m`} 
+          <h3 className="text">
+             Visibility = 
+             {`${weatherData?.visibility}m`} 
             
           </h3>
 
-        <h3>  Humidity = {`${weatherData?.main?.humidity}%`}
+        <h3 className='text'>  Humidity =
+         {`${weatherData?.main?.humidity}%`}
           </h3> 
         </div>
       </div>
